4347f703379cc51312a6b9b5747faefba1c3b883
oluwatobi1 <akintunlesetobi@gmail.com>
2024-03-06 15:33:01 +0100

    s

--- application/app.py
+++ application/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     '''Health check'''
-    return "Hello World(New update)!"
+    return "Hello World(V2)!"
 
 if __name__ == '__main__':
     port = os.getenv("PORT", 8000)

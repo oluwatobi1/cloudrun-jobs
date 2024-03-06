8877e47220e3ee4e61a69f9923c07bacea6b8aed
oluwatobi1 <akintunlesetobi@gmail.com>
2024-03-06 16:34:53 +0100

    ft-setup jobs bash script

---
+++ application/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+set -x
+
+echo "Running my Application"
+python3 app.py
---
+++ application/job.py
@@ -0,0 +1,7 @@
+def main():
+    '''Database migration'''
+    print("running my Job task")
+
+
+if __name__ == '__main__':
+    main()
